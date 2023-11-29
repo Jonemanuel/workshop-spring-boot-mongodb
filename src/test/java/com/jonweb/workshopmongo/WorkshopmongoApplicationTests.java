51f45537bad86a009c83c51e616d90ea1d266a4c
Jonemanuel <jonatasemanuel276@gmail.com>
2023-11-28 21:40:05 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/jonweb/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonweb.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
