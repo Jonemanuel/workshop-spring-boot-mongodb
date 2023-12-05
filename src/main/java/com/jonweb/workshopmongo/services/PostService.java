eaaccef987a2fefdce53f37dc50aa95eaadbc08f
Jonemanuel <jonatasemanuel276@gmail.com>
2023-12-05 17:30:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jonweb/workshopmongo/services/PostService.java
@@ -0,0 +1,29 @@
+package com.jonweb.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jonweb.workshopmongo.domain.Post;
+import com.jonweb.workshopmongo.domain.User;
+import com.jonweb.workshopmongo.repository.PostRepository;
+import com.jonweb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	
+}
+
